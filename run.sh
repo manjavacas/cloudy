c352de442bf647d4eb19753af6993eeff78436e7
Antonio <antomanjavacas@gmail.com>
2024-09-13 14:36:37 +0200

    Updated run script

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
+#!/usr/bin/env bash
+
 for i in $(seq 1 2); do
   gcloud compute instances create "vm-experiment-$i" \
-    --zone=europe-southwest1 \
+    --zone=europe-southwest1-b \
     --machine-type=e2-medium  \
     --image-family=ubuntu-2004-lts \
     --image-project=ubuntu-os-cloud \
     --metadata startup-script='#! /bin/bash
       sudo apt-get update
       sudo apt-get install -y python3-pip
-      pip3 install keras shap
+      pip3 install numpy
       git clone https://github.com/manjavacas/cloudexec.git
-      cd scripts
-      python3 foo.py "Joe"'
+      cd cloudexec/scripts
+      python3 foo.py "Joe" > output.txt'
 done
